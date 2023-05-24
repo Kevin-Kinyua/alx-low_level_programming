ad31cb16fb575cb75f97438cb5bedac752808645
Kevin-Kinyua <w.kevinkinyua@gmail.com>
2023-05-24 14:21:53 -0700

    Task 5: Island Perimeter Technical interview preparation:
    You are not allowed to google anything
    Whiteboard first
    Create a function def island_perimeter(grid): that returns the perimeter of the island described in grid:
    
    grid is a list of list of integers:
    0 represents a water zone
    1 represents a land zone
    One cell is a square with side length 1
    Grid cells are connected horizontally/vertically (not diagonally).
    Grid is rectangular, width and height don’t exceed 100
    Grid is completely surrounded by water, and there is one island (or nothing).
    The island doesn’t have “lakes” (water inside that isn’t connected to the water around the island).
    Requirements:
    
    First line contains #!/usr/bin/python3
    You are not allowed to import any module
    Module and function must be documented

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,39 +1,31 @@
 #!/usr/bin/python3
-"""
-    This is the island_perimeter module.
-"""
+"""Module that calculates the perimeter of an island in a grid."""
 
 
-def neighbour(grid, y, x):
-    """
-        Finds the number of neighbours of a square.
-        Args:
-            grid (list): the matrix.
-            y (int): height parameter.
-            x (int): width parameter.
-    """
-    nb = 4
+def num_water_neighbors(grid, i, j):
+    """Returns the number of water neighbors a cell has in a grid."""
 
-    if (y > 0) and grid[y - 1][x]:
-        nb -= 1
-    if (x > 0) and grid[y][x - 1]:
-        nb -= 1
-    if (y < len(grid) - 1) and grid[y + 1][x]:
-        nb -= 1
-    if (x < len(grid[0]) - 1) and grid[y][x + 1]:
-        nb -= 1
-    return nb
+    num = 0
+
+    if i <= 0 or not grid[i - 1][j]:
+        num += 1
+    if j <= 0 or not grid[i][j - 1]:
+        num += 1
+    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+        num += 1
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
+        num += 1
+
+    return num
 
 
 def island_perimeter(grid):
-    """
-        Finds the island perimeter.
-        Args:
-            grid (list): the matrix.
-    """
+    """Returns the perimeter of the island in grid."""
+
     perimeter = 0
-    for y in range(len(grid)):
-        for x in range(len(grid[0])):
-            if grid[y][x] == 1:
-                perimeter += neighbour(grid, y, x)
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                perimeter += num_water_neighbors(grid, i, j)
+
     return perimeter
